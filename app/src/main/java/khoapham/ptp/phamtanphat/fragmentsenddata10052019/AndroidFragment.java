eb10b0fe4c7f56c221618c1d7aaec0f42af1e463
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-14 20:37:39 +0700

    A

--- app/src/main/java/khoapham/ptp/phamtanphat/fragmentsenddata10052019/AndroidFragment.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragmentsenddata10052019/AndroidFragment.java
@@ -1,11 +1,19 @@
 package khoapham.ptp.phamtanphat.fragmentsenddata10052019;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.OnLifecycleEvent;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +21,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class AndroidFragment extends Fragment {
+public class AndroidFragment extends Fragment implements LifecycleOwner {
 
     EditText edt;
     Button btn;
     View view;
-    String chuoi = "";
+
+    MutableLiveData<String> mutableLiveData = new MutableLiveData<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,12 +40,18 @@ public class AndroidFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        if (!chuoi.equals("")){
-            Toast.makeText(getActivity(), chuoi, Toast.LENGTH_SHORT).show();
-        }
-    }
-    public void setValue(String value){
-        chuoi = value;
+
+        mutableLiveData.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                if (s.length()>0){
+                    Toast.makeText(getActivity(), s, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
+    public void setValue(String value){
+        mutableLiveData.setValue(value);
+    }
 }
--- app/src/main/java/khoapham/ptp/phamtanphat/fragmentsenddata10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragmentsenddata10052019/MainActivity.java
@@ -1,8 +1,10 @@
 package khoapham.ptp.phamtanphat.fragmentsenddata10052019;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 if (chuoi.length() >0){
                     AndroidFragment androidFragment = (AndroidFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentAndroid);
                     if (androidFragment != null && androidFragment.isInLayout()){
+
                         androidFragment.setValue(chuoi);
                     }
                 }
