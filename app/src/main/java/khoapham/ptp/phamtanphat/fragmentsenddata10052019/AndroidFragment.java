261aaeac0000a3970b26637f1e3b7b7c900a4d2b
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-14 19:52:38 +0700

    a

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragmentsenddata10052019/AndroidFragment.java
@@ -0,0 +1,20 @@
+package khoapham.ptp.phamtanphat.fragmentsenddata10052019;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AndroidFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_android, container, false);
+    }
+
+}
